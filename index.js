a7bc2246e41476aef6f1277a914c05b41011521f
JGibbings <j_gibbings@msn.com>
2022-10-17 16:09:18 -0400

    add main menu func

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const menu = require("./utils/menu");
-const mysql = require("mysql2");
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "C3Rulean&",
-    database: "employees",
-  },
-  console.log("Connected to employees database.")
-);
+menu();
--- utils/menu.js
+++ utils/menu.js
@@ -1,3 +1,43 @@
 const { prompt } = require("inquirer");
+const db = require("../db/connection");
+require("console.table");
 
-module.exports = menu;
+const mainMenu = async () => {
+  const { choice } = await prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: ["View", "Add", "Update", "Delete", "Exit"],
+    },
+  ]);
+  switch (choice) {
+    case "View":
+      viewEmployees();
+      break;
+    case "Add":
+      addMenu();
+      break;
+    case "Update":
+      updateMenu();
+      break;
+    case "Delete":
+      deleteMenu();
+      break;
+    case "Exit":
+    default:
+      process.exit();
+  }
+};
+
+const viewMenu = async () => {};
+const viewEmployees = async () => {
+  const [employeeData] = await db.query(`SELECT * FROM employees`);
+  console.table(employeeData);
+  mainMenu();
+};
+const addMenu = async () => {};
+const updateMenu = async () => {};
+const deleteMenu = async () => {};
+
+module.exports = mainMenu;
