389b52fe1afefeec54ffc853b5d55832ab8da0c7
JGibbings <j_gibbings@msn.com>
2022-10-18 12:22:57 -0400

    add all view queries

--- utils/queries/view.js
+++ utils/queries/view.js
@@ -16,20 +16,80 @@ const roles = async () => {
 };
 
 const employees = async () => {
-  const [employeeData] =
-    await db.query(`INSERT INTO departments (id, department_name)
-    VALUES (id:int, 'department_name:varchar');SELECT employees.id, employees.first_name, employees.last_name,
-                    roles.title AS title
-                    departments.department_name AS department
+  const [employees] =
+    await db.query(`SELECT employee.id, employee.first_name, employee.last_name,
+                      roles.title AS title, departments.department_name AS department,
+                      roles.salary AS salary,
+                      concat(manager.first_name, ' ', manager.last_name) AS manager
+                    FROM employees manager, employees employee
+                    LEFT JOIN roles ON employee.role_id = roles.id
+                    LEFT JOIN departments ON roles.department_id = departments.id
+                    WHERE employee.manager_id = manager.id
+                    UNION
+                    SELECT employees.id, employees.first_name, employees.last_name,
+                      roles.title AS title, departments.department_name AS department,
+                      roles.salary AS salary, employees.manager_id AS manager
                     FROM employees
-                    LEFT JOIN roles
-                    ON employees.role_id = role.id;`);
-  console.table(employeeData);
+                    LEFT JOIN roles ON employees.role_id = roles.id
+                    LEFT JOIN departments ON roles.department_id = departments.id
+                    WHERE employees.manager_id IS NULL
+                    ORDER BY id;`);
+  console.table(employees);
 };
 
-const employeesByManager = async () => {};
-const employeesByDepartment = async () => {};
-const budgetByDepartment = async () => {};
+const employeesByManager = async () => {
+  const [employees] =
+    await db.query(`SELECT employee.id, employee.first_name, employee.last_name,
+                    roles.title AS title, departments.department_name AS department,
+                    roles.salary AS salary,
+                    concat(manager.first_name, ' ', manager.last_name) AS manager
+                  FROM employees manager, employees employee
+                  LEFT JOIN roles ON employee.role_id = roles.id
+                  LEFT JOIN departments ON roles.department_id = departments.id
+                  WHERE employee.manager_id = manager.id
+                  UNION
+                  SELECT employees.id, employees.first_name, employees.last_name,
+                    roles.title AS title, departments.department_name AS department,
+                    roles.salary AS salary, employees.manager_id AS manager
+                  FROM employees
+                  LEFT JOIN roles ON employees.role_id = roles.id
+                  LEFT JOIN departments ON roles.department_id = departments.id
+                  WHERE employees.manager_id IS NULL
+                  ORDER BY manager;`);
+  console.table(employees);
+};
+
+const employeesByDepartment = async () => {
+  const [employees] =
+    await db.query(`SELECT employee.id, employee.first_name, employee.last_name,
+                  roles.title AS title, departments.department_name AS department,
+                  roles.salary AS salary,
+                  concat(manager.first_name, ' ', manager.last_name) AS manager
+                FROM employees manager, employees employee
+                LEFT JOIN roles ON employee.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                WHERE employee.manager_id = manager.id
+                UNION
+                SELECT employees.id, employees.first_name, employees.last_name,
+                  roles.title AS title, departments.department_name AS department,
+                  roles.salary AS salary, employees.manager_id AS manager
+                FROM employees
+                LEFT JOIN roles ON employees.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                WHERE employees.manager_id IS NULL
+                ORDER BY department;`);
+  console.table(employees);
+};
+const budgetByDepartment = async () => {
+  const [departments] =
+    await db.query(`SELECT departments.id, departments.department_name,
+                    SUM(roles.salary) AS budget
+                    FROM employees
+                    LEFT JOIN roles ON employees.role_id = roles.id
+                    LEFT JOIN departments ON roles.department_id = departments.id
+                    GROUP BY departments.department_name;`);
+  console.table(departments);
+};
 
 module.exports = {
   departments,
